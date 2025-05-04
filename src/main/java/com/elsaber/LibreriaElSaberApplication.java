371bca7dfa5f577e84115d397cb2a0f7e2e5b68e
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-05-03 22:51:44 -0500

    first commit

---
+++ src/main/java/com/elsaber/LibreriaElSaberApplication.java
@@ -0,0 +1,13 @@
+package com.elsaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaElSaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaElSaberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elsaber/LibreriaElSaberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsaber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaElSaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
